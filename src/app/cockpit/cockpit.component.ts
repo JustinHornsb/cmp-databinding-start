11daf506d9d9c9b58b1432787b52466db2b6177a
Justin Hornsby <justin.hornsby@entelect.co.za>
2022-12-21 13:25:06 +0200

    add local references to cockpit component

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,9 +8,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
-
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -19,14 +18,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
